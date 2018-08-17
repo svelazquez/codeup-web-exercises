aa290a1468b002f2ea14cda13c59b4710144019f
Stephanie Velazquez <svelazquez@Stephanies-MacBook-Air.local>
2018-08-17 10:33:13 -0500

    today's js warmups

--- js/pushups.js
+++ js/pushups.js
@@ -1,21 +1,79 @@
+/*
+    write a conditional statement to find the sign of the product of three numbers
+    Ex: 5, -7, -2
+    output should be -
 
-//function that accepts a number.
-//for loop that iterates from 1 to that number.
-//if any of the numbers are divisible by 3, print fizz
-//if any number is divisible by 5, print buzz
-var num = prompt("Enter ceiling number");
-fizz(num);
+    Ex: -8,4,8
+    output should be +
+ */
+    var x = 5;
+    var y = -7;
+    var z = -2;
+    //product(x,y,z);
+    function product(x,y,z){
+        if((x * y * z) > 0)
+        {
+            console.log("+");
+        } else {
+            console.log("-");
+        }
+    }
 
-function fizz(num){
-        for(var i = 0; i<=num; i++){
-            if(i%3 == 0){
-                console.log(i + ": fizz");
-            } else if(i%5 == 0){
-                console.log(i + ": buzz");
-            } else if(i%3 == 0 && i%5 == 0) {
-                console.log(i + ": fizzbuzz");
-            }else{
-                console.log(i + ": not divisible");
+
+    var nums = [0,-1,4,.6,-.5,];
+    //console.log(sorting(nums));
+    function sorting(arr){
+        return arr.sort().reverse();
+    }
+
+    var eighty1 = [-30,90];
+    var eighty2 = [40,40];
+    var eighty3 = [80, 43];
+    // console.log(isEighty(eighty1));
+    // console.log(isEighty(eighty2));
+    // console.log(isEighty(eighty3));
+    function isEighty (arr) {
+        if (arr[0] + arr[1] == 80 || (arr[0] == 80 || arr[1] == 80)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    var str1 = "Romania";
+    var str2 = "Pyre";
+    // console.log(py(str1));
+    // console.log(py(str2));
+    function py(str){
+        str = str.toLowerCase();
+        if(str.substring(0,2) == "py"){
+            return str;
+        } else {
+            return "py" + str;
+        }
+    }
+
+
+    console.log(repeat("banana"));
+    console.log(repeat("ultimate"));
+    console.log(repeat("more"));
+    function repeat(str){
+        var champion = 0;
+
+        for(var i = 0; i<str.length; i++){
+            var repeat = 0;
+            for(var j =0; j<str.length; j++){
+                if(i!=j && (str[i] === str[j])){
+                    repeat++;
+                }
+            }
+            if(repeat> champion){
+                champion = repeat;
             }
         }
-}
+        return champion;
+    }
+
+
+
+
